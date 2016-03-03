c32d4852f6dbe70f6524ecebc2816f063d0c64a8
micahcourey <micahjcourey@gmail.com>
2016-03-02 21:52:18 -0800

    Add initial project files

---
+++ flask_app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def index(name="Flask"):
+    return "Yo sup {}?!?!?!".format(name)
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    return '{} + {} = {}'.format(num1, num2, num1+num2)
+
+app.run(debug=True, port=8000, host='0.0.0.0')
---
+++ readme.md
@@ -0,0 +1,8 @@
+Practice Flask
+==============
+
+This is my first Flask app. I'm working on learning the basics by building a simple app.
+
+Technologies Used
+----------
+Python, Flask
